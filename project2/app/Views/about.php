1db405d34c8d420a388fdb9d5df5ab38b5e674d3
MRizqiAbdilah <muhammadrizqiabdilah10@gmail.com>
2026-04-10 15:31:26 +0700

    layout templating include header and footer

---
+++ project2/app/Views/about.php
@@ -0,0 +1,8 @@
+<?= $this->extend('layouts/main') ?>
+
+<?= $this->section('content') ?>
+
+<h1 class="text-2xl font-extrabold">About Me</h1>
+<p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, magnam.</p>
+
+<?= $this->endSection() ?>
